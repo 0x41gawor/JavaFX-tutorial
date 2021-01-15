e2844f23f0fd96ced63e7442a33ae294163e8cdb
AndreGolara <41gawor@gmail.com>
2021-01-15 14:23:42 +0100

    5 Labels and Images

--- src/App.java
+++ src/App.java
@@ -1,46 +1,56 @@
 import javafx.application.Application;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+
 
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // With scenes you can add components and layouts to your stages (-> windows)
-        // This episode i will show you how to add a scene to the stage, and later how to add buttons etc. to the scene
-        // Only one scene can be displayed at a time on the stage
-        // Components are also called a nodes
 
-        stage.setTitle("Episode 4 - Scenes");
+        // > Last episode I called it components but these are controls by JavaFX official term
+
+        stage.setTitle("Episode 5 - Images and Labels");
         stage.setWidth(400);
         stage.setHeight(500);
 
-        // Param of Scene.Scene() is the root node of the scene
-        // Here we will give the layoutManager, VBox is a type of the layoutManager
         VBox root = new VBox();
 
-        // Here we can  add some components/node to the root component/node
-        Label label = new Label("This is a text label");
-        Button button = new Button("OK");
+        // I M A G E
 
-        root.getChildren().add(label);
-        root.getChildren().add(button);
-        // root.getChidlren().addAll(label, button);
+        // U can put a link to image
+        ImageView imageView1 = new ImageView("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png");
 
-        // Go google "Scene graph model JavaFX"
+        // Or make Image object
+        Image image = new Image("file:///A:/Ejek/Java/JavaFX/JavaFXTutorial/res/img/tree.jpg");
+
+        ImageView imageView2 = new ImageView(image);
+        root.getChildren().add(imageView1);
+
+        // L A B E L
+
+        // This way u can add an icon to the label, this image is too large to be an icon for this label, but you get it
+        // Remember that without a label imageView will be also displayed because of the previous code
+        Label label = new Label("Text label", imageView1);
+
+        // Bunch of staff u can do with label
+        Label label2 = new Label("Pickles r cool");
+        label2.setTextFill(Color.web("#9042f5"));
+        label2.setFont(new Font("Cambria", 45));
+        label2.setRotate(45);
+
+        root.getChildren().addAll(label, label2);
 
 
         Scene scene = new Scene(root);
 
-        // We can choose what happens with out mouse cursor when it enters the scene
-        scene.setCursor(Cursor.HAND);
-        // Checkout the enum values yourself
-
-        // Assign scene to the stage
         stage.setScene(scene);
 
         stage.show();
