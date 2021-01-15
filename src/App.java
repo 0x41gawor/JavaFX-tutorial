e228717c7d470b247ae6e20192da6ab72c948229
AndreGolara <41gawor@gmail.com>
2021-01-15 13:57:33 +0100

    4 Scenes

--- src/App.java
+++ src/App.java
@@ -1,48 +1,48 @@
 import javafx.application.Application;
-import javafx.stage.Modality;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // With scenes you can add components and layouts to your stages (-> windows)
+        // This episode i will show you how to add a scene to the stage, and later how to add buttons etc. to the scene
+        // Only one scene can be displayed at a time on the stage
+        // Components are also called a nodes
 
-        // Sets window title
-        stage.setTitle("Hello world");
-        // Shows window
+        stage.setTitle("Episode 4 - Scenes");
+        stage.setWidth(400);
+        stage.setHeight(500);
+
+        // Param of Scene.Scene() is the root node of the scene
+        // Here we will give the layoutManager, VBox is a type of the layoutManager
+        VBox root = new VBox();
+
+        // Here we can  add some components/node to the root component/node
+        Label label = new Label("This is a text label");
+        Button button = new Button("OK");
+
+        root.getChildren().add(label);
+        root.getChildren().add(button);
+        // root.getChidlren().addAll(label, button);
+
+        // Go google "Scene graph model JavaFX"
+
+
+        Scene scene = new Scene(root);
+
+        // We can choose what happens with out mouse cursor when it enters the scene
+        scene.setCursor(Cursor.HAND);
+        // Checkout the enum values yourself
+
+        // Assign scene to the stage
+        stage.setScene(scene);
+
         stage.show();
-
-        // Creating window new window
-        Stage stage2 = new Stage();
-        stage2.setTitle("Second window");
-
-        // Set window size, but it's not resizable now
-        stage2.setWidth(400);
-        stage2.setHeight(50);
-
-        // Set position of window, by default they appear in the middle of the screen
-        stage2.setX(0);
-        stage2.setY(0);
-
-        // M O D A L I T Y - how you want to stages(windows) to interact with each other
-        Stage newStage = new Stage();
-        newStage.initModality(Modality.APPLICATION_MODAL);
-        // Modality.APPLICATION_MODAL - other windows can't be used until this one is closed
-        // Modality.NONE - default one
-        // Modality.WINDOW_MODAL - you cannot mess with window that created this window until its closed
-        // You can set a window owner with Stage.initOwner(Stage s)
-
-
-        // S T Y L I N G
-        newStage.initStyle(StageStyle.DECORATED);
-        // StageStyle.DECORATED - default one
-        // StageStyle.TRANSPARENT - window is invisible
-        // StageStyle.UTILITY - only exit option (not minimize or maximize), cool for pop ups, settings etc. Like File -> Settings... in Intellij IDEA
-        // StageStyle.UNDECORATED - shows only content of the window, no frame at all (now it will be invisible)
-        // StageStyle.UNIFIED - removes the border between the decoration area (frame) and the main content area
-
-        stage2.toFront();
-        stage2.show();
-        newStage.show();
     }
 }
