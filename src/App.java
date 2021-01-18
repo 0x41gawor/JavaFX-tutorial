c300bdc89410648e4699b988c5a5cad7494b3601
AndreGolara <41gawor@gmail.com>
2021-01-18 21:41:07 +0100

    8 Buttons and Mnemonics

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -10,23 +11,34 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        stage.setTitle("Episode 7 - CSS styling");
+        stage.setTitle("Episode 8 - Buttons");
         stage.setWidth(400);
         stage.setHeight(500);
 
         VBox root = new VBox();
-        // Set what happens if u uncomment the line below:
-        //root.getStylesheets().add("stylesheets/fire.css");
 
-        Label label = new Label("Cool label");
+        Button button = new Button("_Click me!");
+        button.setStyle("-fx-font-size: 45px");
 
-        Label labelCustom = new Label("Custom label");
-        labelCustom.setId("custom-label"); // Kebab case is used in CSS.
+        //But with this font size button is to small to enclose whole text
+        //button.setText("Reptilians are cool");
+        //So we need to do a text wrapping
+        //button.setWrapText(true);
 
-        Hyperlink link = new Hyperlink("qweqwe");
-        link.setStyle("-fx-background-color: lightcoral; -fx-rotate: 45;");
+        button.setMinSize(20,20);
+        // Uncomment this the line below
+        //button.setPrefSize(300,100);
 
-        root.getChildren().addAll(label, labelCustom, link);
+        // M N E M O N I C  - shortcut buttons its when u press alt and type some letter e.g. alt + t (try it here, in Intellij)
+        button.setMnemonicParsing(true);
+        // We need to put underscore before the letter we want to be shortcut
+
+        button.setOnAction( e -> {
+            System.out.println("Button tapped");
+        });
+
+
+        root.getChildren().addAll(button);
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add("stylesheets/styles.css");
