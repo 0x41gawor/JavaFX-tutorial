f6d3da262129d65317eb49c965abe7a71a5c02ef
AndreGolara <41gawor@gmail.com>
2021-02-03 17:09:36 +0100

    12 - BorderPane

--- src/App.java
+++ src/App.java
@@ -1,42 +1,46 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
-        stage.setTitle("Episode 12 - Vbox and Hbox");
-        stage.setWidth(400);
+        stage.setTitle("Episode 12 - BorderPane");
+        stage.setWidth(500);
         stage.setHeight(500);
+        // Border Pane is a layout manager that allows you to layout your nodes in a five section layout
+        // As you can see in res/img/BorderPane.png
 
-        // Layout Pane, or so called Layout Manager manage the layout of child nodes on the scene.
+        BorderPane root = new BorderPane();
 
-        HBox root = new HBox();
         Button b1 = new Button("One");
         Button b2 = new Button("Two");
         Button b3 = new Button("Three");
         Button b4 = new Button("Four");
+        Button b5 = new Button("Five");
 
-        // Sets amount of width [px] between each element of Hbox
-        root.setSpacing(12);
+        // Normally we do smth like this but with BorderPane does not gonna work bcuz we need to tell where (in which Pane)
+        // we are placing a child node, we need to tell each child node which section it's gonna be
+        //root.getChildren().addAll(b1, b2, b3, b4, b5);
 
-        // Where all the child element will be located on the root node of the Hbox
-        root.setAlignment(Pos.BOTTOM_RIGHT);
+        // So ..
+        root.setCenter(b1);
+        root.setTop(b2);
+        root.setRight(b3);
+        root.setLeft(b4);
+        root.setBottom(b5);
 
-        // Space in px, around the Hbox
-        root.setPadding(new Insets(10,10,10,10));
+        // If u want b2 at the center of the top use this:
+        // BorderPane.setAligment(b2, Pos.CENTER)
+        // Try also this
+        // BorderPane.setAlignment(b3, Pos.BOTTOM_CENTER);
 
-        // Set a padding for a specific node
-        HBox.setMargin(b1,new Insets(50,50,50,50));
-
-
-        root.getChildren().addAll(b1, b2, b3, b4);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
